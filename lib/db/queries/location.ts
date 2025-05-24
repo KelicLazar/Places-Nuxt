ed085e56d4239792953b1a702564ab6b7699f7ee
Lazar Kelić <keliclazar@gmail.com>
2025-05-24 22:52:18 +0200

    :sparkles: move queries to module

---
+++ lib/db/queries/location.ts
@@ -0,0 +1,48 @@
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+
+import type { InsertLocationType } from "../schema";
+
+import db from "..";
+import { location } from "../schema";
+
+const nanoid = customAlphabet("1234567890,qwertyuiopasdfghjklzxcvbnm", 5);
+export async function findLocationByName(existing: InsertLocationType, userId: number) {
+  return db.query.location.findFirst({
+    where: and(
+      eq(location.name, existing.name),
+      eq(location.userId, userId),
+    ),
+  });
+}
+
+export async function findLocationBySlug(slug: string) {
+  return db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  });
+}
+
+export async function findUniqueSlug(slug: string) {
+  let existing = !!(await findLocationBySlug(slug));
+
+  while (existing) {
+    const id = nanoid();
+    console.log(id);
+    const idSlug = `${slug}-${id}`;
+    console.log(idSlug);
+    existing = !!(await findLocationBySlug(idSlug));
+    if (!existing) {
+      return idSlug;
+    }
+  }
+  return slug;
+}
+
+export async function insertLocation(insertable: InsertLocationType, slug: string, userId: number) {
+  const [created] = await db.insert(location).values({
+    ...insertable,
+    slug,
+    userId,
+  }).returning();
+  return created;
+}
