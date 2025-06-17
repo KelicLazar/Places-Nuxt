92def3b571980c855e1845809eb5d9af1445348f
Lazar Kelić <keliclazar@gmail.com>
2025-06-18 00:07:55 +0200

    :sparkles: delete image

--- components/app/image-list.vue
+++ components/app/image-list.vue
@@ -11,8 +11,9 @@ const config = useRuntimeConfig();
 <template>
   <div class="flex mt-2 gap-2">
     <div v-for="image in images" :key="image.id" class="card compact-card h-40 w-64 shring-0 flex bg-base-300 items-center justify-center">
-      <div class="card-body size-full">
-        <img class="size-full object-cover" :src="`${config.public.s3BucketUrl}/${image.key}`" />
+      <div class="card-body size-full flex p-2.5 ">
+        <img class="size-full h-24 flex-1 shrink  object-cover" :src="`${config.public.s3BucketUrl}/${image.key}`" />
+        <slot :image></slot>
       </div>
     </div>
   </div>
