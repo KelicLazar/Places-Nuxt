85e0204f054a33f0ef1842dd535504719ca0d56d
Lazar Kelić <91682497+KelicLazar@users.noreply.github.com>
2025-06-18 00:12:14 +0200

    :sparkles: Delete Images (#85)
     :sparkles: delete image

--- lib/db/queries/location-log-image.ts
+++ lib/db/queries/location-log-image.ts
@@ -1,3 +1,5 @@
+import { and, eq } from "drizzle-orm";
+
 import db from "..";
 import { locationLogImage } from "../schema";
 
@@ -10,4 +12,15 @@ export async function insertLocationLogImage(locationLogId: number, insertable: 
   }).returning();
 
   return inserted;
+}
+
+export async function deleteLocationLogImage(imageId: number, userId: number) {
+  const [deleted] = await db.delete(locationLogImage).where(
+    and(
+      eq(locationLogImage.id, imageId),
+      eq(locationLogImage.userId, userId),
+    ),
+  ).returning();
+
+  return deleted;
 }
